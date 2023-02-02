8b75c4d2f3d6a11ea51d072d6d0d43f571d677f1
JohnMichael0969 <113489915+JohnMichael0969@users.noreply.github.com>
2023-02-02 19:24:29 +0800

    mongo connect

--- API/Mongo/MongoConnect.js
+++ API/Mongo/MongoConnect.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require("mongodb")
 
-const conUrl = 'mongodb+srv://johnmicheael30:<password>@cluster0.4mhl8f1.mongodb.net/test';
+const conUrl = 'mongodb+srv://johnmicheael30:Ctuladonga2020@cluster0.4mhl8f1.mongodb.net/test';
 
 const client = new MongoClient(conUrl, {
     useNewUrlParser: true,
